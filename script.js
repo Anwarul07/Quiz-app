b69d610dc0c41ef9d194ea78c468b84d4138f035
Anwarul07 <mdanwarul064@gmail.com>
2024-05-28 00:09:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const question=[
+const questions = [
     {
-        question:[
-            
-            
-        {text: "shark", correct: false},
-        {text: "blue whale", correct: true},
-        {text: "Elephant", correct: false},
-        {text: "Girrafe", correct: false},
+        question:"Which is the largest animal in the world",
+            answers: [
+                { text: "shark", correct: false },
+                { text: "blue whale", correct: true },
+                { text: "Elephant", correct: false },
+                { text: "Girrafe", correct: false },
         ]
-    }
+    },
+    {
+        question:"Which is the smallest continent in the world",
+        answers: [
+            { text: "Asia", correct: false },
+            { text: "Arctic", correct: false },
+            { text: "Africa", correct: false },
+            { text: "Austrailiya", correct: true },
+    ]
+    },
+    {
+        question:"Which is the largest desert in the world",
+        answers: [
+            { text: "Kalahari", correct: false },
+            { text: "Gobi", correct: false },
+            { text: "Antarctica", correct: true },
+            { text: "Sahara", correct: false },
+           
+    ]
+    },
+    {
+        question:"Which is the smallest country in the world",
+        answers: [
+            { text: "Vaticann City", correct: true },
+            { text: "Bhutan", correct: false },
+            { text: "Nepal", correct: false },
+            { text: "Shri lanka", correct: false },
+    ]
+    },
 ]
